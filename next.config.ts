3c8158eebde46b5280e500719cd1e3db1f3b3d9e
miyazaki <miyazaki.dev01@gmail.com>
2025-11-30 20:39:23 +0900

    update: svgrを導入

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,31 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack(config) {
+    // SVG を React コンポーネントとしてインポートする設定
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule?.test?.test?.(".svg")
+    )!;
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...fileLoaderRule.resourceQuery.not, /url/],
+        },
+        use: ["@svgr/webpack"],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 export default withVanillaExtract(nextConfig);
