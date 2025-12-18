045a714800bccb2c3b92fe41b24b6b58d6761533
miyazaki <miyazaki.dev01@gmail.com>
2025-12-18 21:14:23 +0900

    update: 外部リンクコンポーネントにスタイルが渡せるように修正

--- src/components/elements/Link/ExternalLink/index.tsx
+++ src/components/elements/Link/ExternalLink/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 type Props = {
-  href: string;
+  href: string | undefined;
+  className?: string;
   children: React.ReactNode;
 };
 
-export function ExternalLink({ href, children }: Props) {
+export function ExternalLink({ href, className, children }: Props) {
   return (
-    <a href={href} target="_blank" rel="noreferrer noopener">
+    <a
+      href={href || "#"}
+      target="_blank"
+      rel="noreferrer noopener"
+      className={className}
+    >
       {children}
     </a>
   );
