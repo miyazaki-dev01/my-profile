903a5ef858a7df3b1220aafcff15349acb0b1a41
miyazaki <miyazaki.dev01@gmail.com>
2026-01-05 21:03:53 +0900

    fix: ポートフォリオ一覧のデータ取得処理を修正

--- src/features/portfolio/portfolio-list/api/getPortfolioList.ts
+++ src/features/portfolio/portfolio-list/api/getPortfolioList.ts
@@ -1,12 +1,15 @@
 import { microcmsClient } from "@/libs/microcms";
+import { MICROCMS_ENDPOINT } from "@/constants/microcms";
 import type {
   PortfolioListItem,
   PortfolioList,
 } from "@/features/portfolio/portfolio-list/types/portfolioList";
 
+const endpoint = MICROCMS_ENDPOINT.portfolio;
+
 export async function getPortfolioListData(): Promise<PortfolioList> {
   const portfolioList = await microcmsClient.getAllContents<PortfolioListItem>({
-    endpoint: "portfolio",
+    endpoint: endpoint,
     queries: {
       fields: "id,thumbnail,title,description,articleSlug",
     },
